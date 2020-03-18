aab8d94b04ea615e68448f958392dbfc087166df
dnl092 <drogalsky14@my.whitworth.edu>
2020-03-18 13:40:02 -0700

    added a visualization

--- python_repos.py
+++ python_repos.py
@@ -1,5 +1,8 @@
 import requests
 
+from plotly.graph_objs import bar
+from plotly import offline
+
 #Make an api call and restore the results
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'}
@@ -7,19 +10,38 @@ r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 #store API response in a variable
 response_dict = r.json()
-print(f"Total repositories: {response_dict['total_count']}")
-
-#Explore information about the repos
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
+repo_names, stars = [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
 
-# Examine the first repo
-repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-print(f"Name: {repo_dict['name']}")
-print(f"Owner: {repo_dict['owner']['login']}")
-print(f"Stars: {repo_dict['stargazers_count']}")
-print(f"Repository: {repo_dict['html_url']}")
-print(f"Created: {repo_dict['created_at']}")
-print(f"Updated: {repo_dict['updated_at']}")
-print(f"Description: {repo_dict['description']}")
+#make visualization
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'marker': {
+        'color': 'rgb(60, 100, 150)',
+        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    'opacity': 0.6,
+}]
+
+my_layout ={
+    'title': 'Most-Starred python projects on GitHub',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'Repository',
+        'titlefont': {'size': 24},
+        'tickfont' : {'size': 14},
+    },
+    'yaxis': {
+        'title': 'Stars',
+        'titlefont': {'size': 24},
+        'tickfont' : {'size': 14},
+    }
+}
+
+fig = {'data': data, 'layout':my_layout}
+offline.plot(fig, filename='python_repos.html')
